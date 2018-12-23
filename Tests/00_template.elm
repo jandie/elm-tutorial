be2e8152bd1a9147a0d976b0cb21ea7a4e651e71
jandie <eidnaj@live.nl>
2018-12-23 12:34:27 +0100

    Default ellie template

---
+++ Tests/00_template.elm
@@ -0,0 +1,45 @@
+module Main exposing (main)
+
+import Html exposing (Html, button, div, text)
+import Html.Events exposing (onClick)
+
+
+type alias Model =
+    { count : Int }
+
+
+initialModel : Model
+initialModel =
+    { count = 0 }
+
+
+type Msg
+    = Increment
+    | Decrement
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Increment ->
+            { model | count = model.count + 1 }
+
+        Decrement ->
+            { model | count = model.count - 1 }
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ button [ onClick Increment ] [ text "+1" ]
+        , div [] [ text <| toString model.count ]
+        , button [ onClick Decrement ] [ text "-1" ]
+        ]
+
+main : Program Never Model Msg
+main =
+    Html.beginnerProgram
+        { model = initialModel
+        , view = view
+        , update = update
+        }
